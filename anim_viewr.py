9967fa8b160c5d08639f39cf5d74b0c77b5b0696
jjuncode <jjun.code@gmail.com>
2023-09-23 15:31:31 +0900

    Repeat Animation
    Ani 반복출력

--- anim_viewr.py
+++ anim_viewr.py
@@ -7,30 +7,30 @@ character = load_image('sprite.png')
 
 center = get_canvas_width()/2,get_canvas_height()/2 # window center
 
-x,y = 0,center[1]   # character pos
+x,y = 400,center[1]   # character pos
 
 class Ani:
     offset_x = 74       # ani offset x
     offset_y = 80       # ani offset y 
     cur_frame =0
-    max_frame = [8,8,8,8,3,1,3,3]    # sprite max frame 
+    max_frame = [9,9,9,9,2,0,2,2]    # sprite max frame 
     cur_ani = 0         # cur ani ( from img_bottom )
 
 class Distance:
     distn =0
 
-def MoveCharacter():
+def DrawAnimation():
     global x,y
     clear_canvas()
     background.draw(center[0], center[1])
     character.clip_draw(Ani.cur_frame*Ani.offset_x,Ani.cur_ani*Ani.offset_y,Ani.offset_x,Ani.offset_y
                         ,x,y)
     update_canvas()
-    x += 5
  
 def SetFrame():
     Ani.cur_frame += 1
-    if (Ani.cur_frame >= Ani.max_frame[Ani.cur_ani]):
+    if (Ani.cur_ani > 7): Ani.cur_ani = 0
+    if (Ani.cur_frame > Ani.max_frame[Ani.cur_ani]):
         Ani.cur_frame =0
 
     Distance.distn +=5
@@ -42,6 +42,6 @@ def SetFrame():
 
 while (1):
     SetFrame()
-    MoveCharacter()
+    DrawAnimation()
     get_events()
 close_canvas()
