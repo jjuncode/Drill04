4fa4cf50cde281c2b2a06f9d4d6cf0f7135f7488
jjuncode <jjun.code@gmail.com>
2023-09-25 13:22:17 +0900

    Move Character UP,DOWN,LEFT,RIGHT

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -8,6 +8,8 @@ character = load_image('sprite.png')
 center = get_canvas_width()/2,get_canvas_height()/2 # window center
 
 x,y = 400,center[1]   # character pos
+move_speed = 50
+quit = False
 
 class Ani:
     offset_x = 74       # ani offset x
@@ -16,13 +18,14 @@ class Ani:
     max_frame = [9,9,9,9,2,0,2,2]    # sprite max frame 
     cur_ani = 0         # cur ani ( from img_bottom )
 
-    def FrameReset():
-        Ani.cur_frame =0
+class Dir:
+    x,y = 0,0
 
 def DrawAnimation():
     global x,y
     clear_canvas()
-    background.draw(x,y,)
+    background.draw(center[0],center[1])
+    MoveCharacter()
     character.clip_draw(Ani.cur_frame*Ani.offset_x,Ani.cur_ani*Ani.offset_y,Ani.offset_x,Ani.offset_y
                         ,x,y)
     update_canvas()
@@ -31,16 +34,43 @@ def SetFrame():
     Ani.cur_frame += 1
 
     if (Ani.cur_frame > Ani.max_frame[Ani.cur_ani]):    # Frame reset
-        Ani.FrameReset()
+        Ani.cur_frame =0
 
     delay(0.1)
 
-def handle_event():
-    pass
+def MoveCharacter():
+    global x,y
+    x += Dir.x * move_speed;
+    y += Dir.y * move_speed;
 
-while (1):
+def handle_event():
+    global quit,dir
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            quit = True
+        if event.type == SDL_KEYDOWN:
+            if event.key ==SDLK_UP:
+                Dir.y +=1
+            elif event.key == SDLK_DOWN:
+                Dir.y -=1
+            elif event.key == SDLK_LEFT:
+                Dir.x -=1
+            elif event.key == SDLK_RIGHT:
+                Dir.x +=1
+        if event.type == SDL_KEYUP:
+            if event.key ==SDLK_UP:
+                Dir.y -=1
+            elif event.key == SDLK_DOWN:
+                Dir.y +=1
+            elif event.key == SDLK_LEFT:
+                Dir.x +=1
+            elif event.key == SDLK_RIGHT:
+                Dir.x -=1
+
+while (not quit):
     SetFrame()
+    handle_event()
     DrawAnimation()
-    get_events()
-    
+
 close_canvas()
